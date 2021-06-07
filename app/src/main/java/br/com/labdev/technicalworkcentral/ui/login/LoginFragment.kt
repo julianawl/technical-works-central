01fdac9c9c431186496ba97c261c6301710bd5e0
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-06-07 20:33:32 -0300

    Implementing Login Fragment

---
+++ app/src/main/java/br/com/labdev/technicalworkcentral/ui/login/LoginFragment.kt
@@ -0,0 +1,21 @@
+package br.com.labdev.technicalworkcentral.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.labdev.technicalworkcentral.R
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
